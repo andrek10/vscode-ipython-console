f24c7a12911f9e376b7e9e496df85e204a5e4af9
Andreas Klausen <andreas.klausen0@gmail.com>
2020-05-22 14:30:48 +0200

    Importing code from vscode-jupyter-qtconsole

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,98 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+'use strict';
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+//Class to keep terminal references, and run IPython commands
+class CodeManager_1 {
+    //field - initialize constructor .this items
+    terminal
+    terminal_ahk
+    terminalsOpened
+
+    //Constructor
+    constructor() {
+        this.terminal = null;
+        this.terminal_ahk = null;
+        this.terminalsOpened = null;
+    }
+
+    //Functions
+    runPython() {
+        // Get current editor
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+        
+        // Get filename of current file
+        var filename = editor.document.fileName;
+        
+        //Make IPython Command
+        var ipyCom = "run ".concat(filename);
+        // var ipyCom = "execfile(r\"";
+        // ipyCom = ipyCom.concat(filename);
+        // ipyCom = ipyCom.concat("\");");
+
+        //Send IPython command
+        this.terminal.show();
+        this.terminal.sendText(ipyCom, false);
+
+        //Send enterkey via AutoHotKey
+        this.terminal_ahk.sendText("sendEnter.ahk", true);
+    }
+    makeTerminal() {
+
+        // var a = vscode.window.showInputBox({prompt : "Close Terminal? y/n",
+        //                                    ignoreFocusOut : true});
+
+        if (this.terminalsOpened == 1) {
+            try {
+                this.terminal_ahk.dispose()
+            }
+            catch (e) {
+                console.log("Terminal ahkTerminal already closed");
+            }
+
+            try {
+                this.terminal.dispose()
+            }
+            catch (e) {
+                console.log("Terminal IPython alreacy closed");
+            }
+        }
+
+        this.terminalsOpened = 1;
+        // Make the AutoHotKey terminal
+        this.terminal_ahk = vscode.window.createTerminal("ahkTerminal");
+        this.terminal_ahk.sendText("cd %HOMEPATH%", true);
+        //this.terminal_ahk.sendText("cd ak-ipython\\src")
+        this.terminal_ahk.sendText("cd .vscode\\extensions\\vscode-jupyter-qtconsole\\src")
+
+        // Make IPython terminal
+        this.terminal = vscode.window.createTerminal("IPython");
+        this.terminal.show()
+    }
+};
+
+//Extension initializer, and command register
 export function activate(context: vscode.ExtensionContext) {
+    //Make new class instance
+    const codeManager = new CodeManager_1();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-ipython-console" is now active!');
+    //Command to start terminals
+    var disposable = vscode.commands.registerCommand('extension.startConsole', () => {
+        // vscode.window.showInformationMessage('Starting a console');
+        codeManager.makeTerminal();
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-ipython-console.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+    //Command to run IPython commands
+    var disposable = vscode.commands.registerCommand('extension.runPython', () => {
+        codeManager.runPython();
+    });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-ipython-console!');
-	});
-
-	context.subscriptions.push(disposable);
+    //Push the functions to VSCode
+    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+}
